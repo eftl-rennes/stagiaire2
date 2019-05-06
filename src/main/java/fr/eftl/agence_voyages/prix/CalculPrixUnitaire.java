aff6743e0a8a9125a4c3d6ba56054297888df7ca
unknown <git commit Initial>
2019-05-06 15:35:12 +0200

    n'omporte quoi

---
+++ src/main/java/fr/eftl/agence_voyages/prix/CalculPrixUnitaire.java
@@ -0,0 +1,16 @@
+package fr.eftl.agence_voyages.prix;
+
+public class CalculPrixUnitaire {
+	private String depart;
+	private String arrivee;
+	
+	public CalculPrixUnitaire(String depart, String arrivee) {
+		this.depart=depart;
+		this.arrivee=arrivee;
+	}
+	
+	public Integer generer() {
+		return null;
+		
+	}
+}
